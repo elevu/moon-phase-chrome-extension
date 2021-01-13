25f23e843352873ed6cda4e64a227284ee800520
elevu <e.maccacaro@gmail.com>
2021-01-13 16:05:42 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { DateTime } = require("luxon");
+  const minutesPerDay = 42524.0462963;
+  const moonCycleDays = 29.53058770576;
+  const minutesPerMoonCycle = moonCycleDays * minutesPerDay;
+  const firstMoonOf2021 = DateTime.fromISO("2021-01-13T05:02:00", {
+    zone: "UTC"
+  });
+
+  const secondsFromNewMoon = (now = DateTime.local(), knowNewMoon = firstMoonOf2021 ) => {
+    const minutesFromFirstNewMoon = now
+        .diff(knowNewMoon, "minutes")
+        .toObject().minutes;
+    const currentMoonMinutes = minutesFromFirstNewMoon % minutesPerMoonCycle;
+    return (currentMoonMinutes * 100) / minutesPerMoonCycle;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Moon is</h1>
+        <h2>{Math.round(secondsFromNewMoon())}%</h2>
+      </div>
   );
 }
+
 
 export default App;
